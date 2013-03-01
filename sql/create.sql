3fedb79ef35b88d427b6f5361631d692593cc61c
Matthew Hasler <matthew@ganzogo.com>
2013-03-01 10:40:37 +0000

    Added static files.

--- sql/create.sql
+++ sql/create.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS {{ project_name }};
 CREATE DATABASE {{ project_name }};
 GRANT ALL ON {{ project_name }}.* TO {{ project_name }} IDENTIFIED BY '{{ project_name }}';
 GRANT ALL ON {{ project_name }}.* TO '{{ project_name }}'@'localhost' IDENTIFIED BY '{{ project_name }}';
